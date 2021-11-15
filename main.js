dcdd5b367ac2210e988cecddc1a7b3383bcec768
nirmal <nirmalkk0707@gmail.com>
2021-11-15 21:15:29 +0530

    L5 End

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 123, color: 'green'},
+                { id: 124, color: 'blue'}
+            ],
+            sizes: [10, 12, 14, 16]
         }
     }
 })
